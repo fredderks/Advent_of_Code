9756824f6eed397b51fe15579312919bdaf8e723
Fred Derks <frederiquederks@gmail.com>
2019-12-16 11:58:39 +0100

    Initial Commit

--- Day 5/Day5.py
+++ Day 5/Day5.py
@@ -5,78 +5,197 @@ code = [int(i) for i in code_str]
 print(code)
 
 # region --- Assignment 1 ---
-intcode = code.copy()
-# intcode_list1[1] = 12
-# intcode_list1[2] = 2
-# print(max(code))
-
-
-# for i in range(0, len(intcode_list1), 4):
-i = 0
-while True:
-    instruction = intcode[i]
-    print("instruction:", instruction, '- at index:', intcode.index(instruction))
-    if instruction == 1:
-        intcode[intcode[i + 3]] = intcode[intcode[i + 1]] + intcode[intcode[i + 2]]
-        i += 4
-        continue
-    if instruction == 2:
-        intcode[intcode[i + 3]] = intcode[intcode[i + 1]] * intcode[intcode[i + 2]]
-        i += 4
-        continue
-    if instruction == 3:
-        result = int(input('Please provide test input: '))
-        intcode[intcode[i + 1]] = result                    # stores your input at the index given by the parameter
-        i += 2
-        continue
-    elif instruction == 4:
-        print(intcode[intcode[i + 2]])
-        i += 2
-        continue
-    elif instruction == 99:
-        break
-    else:
-        print('Parameter mode time!')
-        instruction = str(instruction)
-        opcode = int(instruction[-2:])
-        param1_mode = int(instruction[-3])
-        param2_mode = int(instruction[-4])
-        if len(instruction) > 4:
-            param3_mode = int(instruction[-5])
-        else:
-            param3_mode = 0
-
-        # print('Opcode:',opcode,"param1",param1_mode,"param2",param2_mode,"param3",param3_mode)
-        if not param1_mode:
-            param1 = intcode[intcode[i + 1]]
-        elif param1_mode:
-            param1 = intcode[i + 1]
-        if not param2_mode:
-            param2 = intcode[intcode[i + 2]]
-        elif param2_mode:
-            param2 = intcode[i + 2]
-
-        # print('param1',param1,'param2',param2)
-
-        if opcode == 1:
-            result = param1 + param2
-            if not param3_mode:
-                intcode[intcode[i + 3]] = result
-            elif param3_mode:
-                intcode[i + 3] = result
-            i += 4
-            continue
-        elif opcode == 2:
-            result = param1 * param2
-            if not param3_mode:
-                intcode[intcode[i + 3]] = result
-            elif param3_mode:
-                intcode[i + 3] = result
-            i += 4
-            continue
-        else:
-            break
 print(" --- Assignmnent 1 --- ")
-print(intcode)
-# Correct: 2842648
+intcode = code.copy()
+
+i = 0
+
+def get_param1(mode):
+    if not mode:
+        param1 = intcode[intcode[i + 1]]
+    elif mode and instr_len > 2:
+        param1 = intcode[i + 1]
+    return param1
+
+def get_param2(mode):
+    if not mode:
+        param2 = intcode[intcode[i + 2]]
+    elif mode and instr_len > 3:
+        param2 = intcode[i + 2]
+    return param2
+
+# while True:
+#     param1_mode = 0
+#     param2_mode = 0
+#     param3_mode = 0
+#     instruction = intcode[i]
+#     # print("instruction:", instruction, 'at index:', intcode.index(instruction), end = "")
+#     instruction = str(instruction)
+#     instr_len = len(instruction)
+#     opcode = int(instruction[-2:])
+#     if instr_len > 2:
+#         param1_mode = int(instruction[-3])
+#     if instr_len > 3:
+#         param2_mode = int(instruction[-4])
+#     if instr_len > 4:
+#         param3_mode = int(instruction[-5])
+#     # print(" - opcode:", opcode, "  C:", param1_mode, "  B:", param2_mode, "  A:", param3_mode)
+#
+#     if opcode == 1:
+#         param1 = get_param1(param1_mode)
+#         param2 = get_param2(param2_mode)
+#         result = param1 + param2
+#         if not param3_mode:
+#             intcode[intcode[i + 3]] = result
+#         elif param3_mode:
+#             intcode[i + 3] = result
+#         i += 4
+#         continue
+#
+#     elif opcode == 2:
+#         param1 = get_param1(param1_mode)
+#         param2 = get_param2(param2_mode)
+#         result = param1 * param2
+#         if not param3_mode:
+#             intcode[intcode[i + 3]] = result
+#         elif param3_mode:
+#             intcode[i + 3] = result
+#         i += 4
+#         continue
+#
+#     elif opcode == 3:
+#         result = int(input('Please provide test input: '))
+#         intcode[intcode[i + 1]] = result
+#         i += 2
+#         continue
+#
+#     elif opcode == 4:
+#         param1 = get_param1(param1_mode)
+#         if param1 == 0:
+#             print("The test was successful!")
+#         else:
+#             print("Output code:",param1)
+#         i += 2
+#         continue
+#
+#     elif opcode == 99:
+#         break
+#
+#     else:
+#         print(opcode, "is not a valid opcode")
+#         break
+
+# correct: 9961446
+# endregion
+
+# region --- Assignment 2 ---
+print(" --- Assignmnent 2 --- ")
+while True:
+    param1_mode = 0
+    param2_mode = 0
+    param3_mode = 0
+    instruction = intcode[i]
+    # print("instruction:", instruction, 'at index:', intcode.index(instruction), end = "")
+    instruction = str(instruction)
+    instr_len = len(instruction)
+    opcode = int(instruction[-2:])
+    if instr_len > 2:
+        param1_mode = int(instruction[-3])
+    if instr_len > 3:
+        param2_mode = int(instruction[-4])
+    if instr_len > 4:
+        param3_mode = int(instruction[-5])
+    # print(" - opcode:", opcode, "  C:", param1_mode, "  B:", param2_mode, "  A:", param3_mode)
+
+    if opcode == 1:
+        param1 = get_param1(param1_mode)
+        param2 = get_param2(param2_mode)
+        result = param1 + param2
+        if not param3_mode:
+            intcode[intcode[i + 3]] = result
+        elif param3_mode:
+            intcode[i + 3] = result
+        i += 4
+        continue
+
+    elif opcode == 2:
+        param1 = get_param1(param1_mode)
+        param2 = get_param2(param2_mode)
+        result = param1 * param2
+        if not param3_mode:
+            intcode[intcode[i + 3]] = result
+        elif param3_mode:
+            intcode[i + 3] = result
+        i += 4
+        continue
+
+    elif opcode == 3:
+        result = int(input('Please provide test input: '))
+        intcode[intcode[i + 1]] = result
+        i += 2
+        continue
+
+    elif opcode == 4:
+        param1 = get_param1(param1_mode)
+        print("Output code:", param1)
+        i += 2
+        continue
+
+    elif opcode == 5:
+        param1 = get_param1(param1_mode)
+        param2 = get_param2(param2_mode)
+        if param1 != 0:
+            i = param2
+        else:
+            i += 3
+        continue
+
+    elif opcode == 6:
+        param1 = get_param1(param1_mode)
+        param2 = get_param2(param2_mode)
+        if param1 == 0:
+            i = param2
+        else:
+            i += 3
+        continue
+
+    elif opcode == 7:
+        param1 = get_param1(param1_mode)
+        param2 = get_param2(param2_mode)
+
+        if param1 < param2:
+            store = 1
+        else:
+            store = 0
+
+        if not param3_mode:
+            intcode[intcode[i + 3]] = store
+        elif param3_mode:
+            intcode[i + 3] = store
+        i += 4
+        continue
+
+    elif opcode == 8:
+        param1 = get_param1(param1_mode)
+        param2 = get_param2(param2_mode)
+
+        if param1 == param2:
+            store = 1
+        else:
+            store = 0
+
+        if not param3_mode:
+            intcode[intcode[i + 3]] = store
+        elif param3_mode:
+            intcode[i + 3] = store
+        i += 4
+        continue
+
+    elif opcode == 99:
+        print("Halting the program!")
+        break
+
+    else:
+        print(opcode, "is not a valid opcode")
+        break
 # endregion
